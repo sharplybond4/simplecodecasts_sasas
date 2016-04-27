18949a1b67dade386a3cb205c5764168135ccb1f
sharplybond4 <sharplybond4@gmail.com>
2016-04-27 20:13:48 +0000

    Added devise gem for users

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,6 +6,6 @@ class ContactMailer < ActionMailer::Base
        @email = email
        @body = body
        
-       mail(from: email, subject: 'Contact Form Message From Dev Finder')
+       mail(from: email, subject: 'Contact Form Message')
     end
 end
